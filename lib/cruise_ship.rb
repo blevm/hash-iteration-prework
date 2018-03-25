d2bef40e0aa36bea50b61f5c8eeb98402561feef
Michelle Blevins <mjblevins7@gmail.com>
2018-03-25 23:54:06 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,7 +8,7 @@ passengers = {
 
 def select_winner(passengers)
   # add the code snippet here!
-  winner = []
+  winner = " "
   passengers.each do |key,value|
     if key == :suite_a && value.starts_with("A")
       winner = value
